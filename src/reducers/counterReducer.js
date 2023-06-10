9324442897d106be35225a84cbd97a9f87f606cc
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-06-10 23:53:33 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,12 @@
 const counterReducer = (state,action) =>{
-
+switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      default:
+        return state;
+    }
 }
 
 export {counterReducer}
